87575d5ea92fa6d4062f04e08e40aff632277964
suyeno <realsu_yeonini418@naver.com>
2023-08-08 19:49:46 +0900

    introduce_random_image

--- app/src/main/java/com/example/introduce/SignInActivity.kt
+++ app/src/main/java/com/example/introduce/SignInActivity.kt
@@ -1,14 +1,19 @@
 package com.example.introduce
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var sign_up: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
@@ -16,29 +21,35 @@ class SignInActivity : AppCompatActivity() {
         val id_user = findViewById<EditText>(R.id.user_name)
         val password_user = findViewById<EditText>(R.id.user_password)
 
+        val signup_btn = findViewById<Button>(R.id.signup_btn)
         val login_btn = findViewById<Button>(R.id.login_btn)
 
         login_btn.setOnClickListener {
 
             val id = id_user.text.toString()
             val password = password_user.text.toString()
-            if(id.isNotEmpty() && password.isNotEmpty()) // id 와 password 가 채워져있을떄 동작
-            {
+            if (id.isNotEmpty() && password.isNotEmpty()) {
                 val intent_login = Intent(this, HomeActivity::class.java)
-                intent.putExtra("id", id) // 아이디를 Extra 로 전달
+                intent_login.putExtra("id", id)
                 startActivity(intent_login)
-                Toast.makeText(this, " 로그인 성공 ",Toast.LENGTH_LONG).show() // Toast 로 팝업 생성
-
             } else {
-                Toast.makeText(this,"아이디 / 비밀번호를 확인해주세요.",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "아이디 / 비밀번호를 확인해주세요.", Toast.LENGTH_LONG).show()
             }
-
         }
 
-        val signup_btn = findViewById<Button>(R.id.signup_btn)
         signup_btn.setOnClickListener {
             val intent_signup = Intent(this, SignUpActivity::class.java)
-            startActivity(intent_signup)
+            sign_up.launch(intent_signup)
+        }
+
+        sign_up = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                val idData = data?.getStringExtra("signUpId") ?: ""
+                id_user.setText(idData)
+                val passwordData = data?.getStringExtra("signUpPassword") ?: ""
+                password_user.setText(passwordData)
+            }
         }
     }
 }
--- app/src/main/java/com/example/introduce/SignUpActivity.kt
+++ app/src/main/java/com/example/introduce/SignUpActivity.kt
@@ -1,6 +1,6 @@
 package com.example.introduce
 
-
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,33 +9,32 @@ import android.widget.EditText
 import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val id_user =findViewById<EditText>(R.id.user_id)
+        val id_user = findViewById<EditText>(R.id.user_id)
         val password_user = findViewById<EditText>(R.id.user_pwd)
         val name_user = findViewById<EditText>(R.id.user_name)
 
-        val back_button = findViewById<Button>(R.id.signup_btn)
+        val signup_btn = findViewById<Button>(R.id.signup_btn)
 
-        back_button.setOnClickListener{
-
+        signup_btn.setOnClickListener {
             val id = id_user.text.toString()
             val password = password_user.text.toString()
             val name = name_user.text.toString()
 
-            if(id.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty())
-            {
-                val intent = Intent(this,SignInActivity::class.java)
+            if (id.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()) {
+                val intent = Intent()
+                intent.putExtra("signUpId", id)
+                intent.putExtra("signUpPassword", password)
+                setResult(Activity.RESULT_OK, intent)
                 finish()
-                Toast.makeText(this,"회원가입 완료",Toast.LENGTH_LONG).show()
-            }else {
-                Toast.makeText(this,"입력되지않은 정보가 있습니다.",Toast.LENGTH_LONG).show()
-
+                Toast.makeText(this, "회원가입 완료", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_LONG).show()
             }
         }
-
-
     }
 }
