92493e87bc83b877c54b7c7468d2231088aced9d
suyeno <realsu_yeonini418@naver.com>
2023-08-08 19:49:46 +0900

    introduce_random_image

--- app/src/main/java/com/example/introduce/HomeActivity.kt
+++ app/src/main/java/com/example/introduce/HomeActivity.kt
@@ -3,9 +3,11 @@ package com.example.introduce
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,16 +15,23 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val user_id = findViewById<TextView>(R.id.user_id_extra)
+        val imageView = findViewById<ImageView>(R.id.image)
         val finish_btn = findViewById<Button>(R.id.button)
 
-        val id = intent.getStringExtra("id")
-        user_id.setText(id)
+        val randomImageNames = arrayOf("one", "two", "three", "four", "five")
+        val randomIndex = Random().nextInt(randomImageNames.size)
+        val randomImageName = randomImageNames[randomIndex]
+        val imageResourceId = resources.getIdentifier(randomImageName, "drawable", packageName)
+        imageView.setImageResource(imageResourceId)
 
-        finish_btn.setOnClickListener{
+        val id = intent.getStringExtra("id")
+        user_id.text = id
+
+        finish_btn.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
             finish()
-            Toast.makeText(this, "${id}",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, id, Toast.LENGTH_LONG).show()
         }
     }
 }
